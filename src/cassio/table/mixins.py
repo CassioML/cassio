501e339b549671bf9845d773a2b61d4d21efe279
Stefano Lottini <stefano.lottini@datastax.com>
2023-07-12 22:27:19 +0200

    reduced protocol clutter, full typing

--- src/cassio/table/mixins.py
+++ src/cassio/table/mixins.py
@@ -1,78 +1,59 @@
-from typing import Any, List, Dict, Protocol, Set, Tuple, Type
+from typing import Any, List, Dict, Iterable, Optional, Protocol, Set, Tuple, Type
 
 from cassio.table.table_types import ColumnSpecType, RowType, SessionType
+from cassio.table.base_table import BaseTable
 
 
-class BaseTableMixinProtocol(Protocol):
-    @classmethod
-    def _schema_pk(cls) -> List[ColumnSpecType]:
-        ...
-
-    @classmethod
-    def _schema_cc(cls) -> List[ColumnSpecType]:
-        ...
-
-    @classmethod
-    def _schema_da(cls) -> List[ColumnSpecType]:
-        ...
-
-
-class BaseTableMixin:
+class BaseTableMixin(BaseTable):
     """All other mixins should inherit from this one."""
 
-    @classmethod
-    def _schema_pk(cls: Type[BaseTableMixinProtocol]) -> List[ColumnSpecType]:
-        return super()._schema_pk()
-
-    @classmethod
-    def _schema_cc(cls: Type[BaseTableMixinProtocol]) -> List[ColumnSpecType]:
-        return super()._schema_cc()
-
-    @classmethod
-    def _schema_da(cls: Type[BaseTableMixinProtocol]) -> List[ColumnSpecType]:
-        return super()._schema_da()
+    pass
 
 
 class ClusteredMixin(BaseTableMixin):
     @classmethod
-    def _schema_pk(cls):
+    def _schema_pk(cls) -> List[ColumnSpecType]:
         return [
             "partition_id",
         ]
 
     @classmethod
-    def _schema_cc(cls):
+    def _schema_cc(cls) -> List[ColumnSpecType]:
         return cls._schema_row_id()
 
-    def delete_partition(self, partition_id):
+    def delete_partition(self, partition_id: str) -> None:
         delete_p_cql = "DELETE_PARTITION: (partition_id)"
         delete_p_cql_vals = (partition_id,)
         self.execute_cql(delete_p_cql, delete_p_cql_vals)
+        return
 
-    def get_partition(self, partition_id, n=None):
+    def get_partition(
+        self, partition_id: str, n: Optional[int] = None
+    ) -> Iterable[RowType]:
+        get_p_cql_vals: Tuple[Any, ...] = tuple()
         if n is None:
             get_p_cql = "GET_PARTITION: (partition_id)"
             get_p_cql_vals = (partition_id,)
         else:
             get_p_cql = "GET_PARTITION: (partition_id) LIMIT (n)"
             get_p_cql_vals = (partition_id, n)
-        self.execute_cql(get_p_cql, get_p_cql_vals)
+        return self.execute_cql(get_p_cql, get_p_cql_vals)
 
 
 class MetadataMixin(BaseTableMixin):
     @classmethod
-    def _schema_da(cls):
+    def _schema_da(cls) -> List[ColumnSpecType]:
         return super()._schema_da() + [
             "metadata_s",
             "metadata_n",
             "metadata_tags",
         ]
 
-    def db_setup(self):
+    def db_setup(self) -> None:
         super().db_setup()
         self.execute_cql("CREATE_METADATA_SAIs")
 
-    def _split_metadata(self, md_dict):
+    def _split_metadata(self, md_dict: Dict[str, Any]) -> Dict[str, Any]:
         # TODO: more care about types here
         stringy_part = {k: v for k, v in md_dict.items() if isinstance(v, str)}
         numeric_part = {
@@ -97,7 +78,7 @@ class MetadataMixin(BaseTableMixin):
             "metadata_tags": nully_part,
         }
 
-    def put(self, /, **kwargs):
+    def put(self, /, **kwargs: Any):
         if "metadata" in kwargs:
             new_metadata_fields = self._split_metadata(kwargs["metadata"])
         else:
@@ -113,28 +94,28 @@ class MetadataMixin(BaseTableMixin):
 
 class VectorMixin(BaseTableMixin):
     @classmethod
-    def _schema_da(cls):
+    def _schema_da(cls) -> List[ColumnSpecType]:
         return super()._schema_da() + ["vector"]
 
-    def db_setup(self):
+    def db_setup(self) -> None:
         super().db_setup()
         self.execute_cql("CREATE_VECTOR_SAI")
 
-    def ann_search(self, vector, **kwargs):
+    def ann_search(self, vector: List[float], **kwargs: Any) -> Iterable[RowType]:
         raise NotImplementedError
 
 
-class ElasticKeyMixin:
-    def __init__(self, *pargs, keys, **kwargs):
+class ElasticKeyMixin(BaseTableMixin):
+    def __init__(self, *pargs: Any, keys: List[str], **kwargs: Any) -> None:
         self.keys = keys
         self.key_desc = "/".join(self.keys)
         super().__init__(*pargs, **kwargs)
 
     @staticmethod
-    def _serialize_key_vals(key_vals: List[str]):
+    def _serialize_key_vals(key_vals: List[Any]) -> str:
         return str(key_vals)
 
-    def _split_row_args(self, arg_dict):
+    def _split_row_args(self, arg_dict: Dict[str, Any]) -> Tuple[str, Dict[str, Any]]:
         # split in key/nonkey from a kwargs dict
         # and represent the former as one field
         key_args = {k: v for k, v in arg_dict.items() if k in self.keys}
@@ -146,21 +127,21 @@ class ElasticKeyMixin:
         other_kwargs = {k: v for k, v in arg_dict.items() if k not in self.keys}
         return key_vals, other_kwargs
 
-    def delete(self, /, **kwargs):
+    def delete(self, /, **kwargs: Any) -> None:
         key_vals, other_kwargs = self._split_row_args(kwargs)
         super().delete(key_desc=self.key_desc, key_vals=key_vals, **other_kwargs)
 
-    def get(self, /, **kwargs):
+    def get(self, /, **kwargs: Any) -> RowType:
         key_vals, other_kwargs = self._split_row_args(kwargs)
         # TODO: unpack the key
         return super().get(key_desc=self.key_desc, key_vals=key_vals, **other_kwargs)
 
-    def put(self, /, **kwargs):
+    def put(self, /, **kwargs: Any) -> None:
         key_vals, other_kwargs = self._split_row_args(kwargs)
         super().put(key_desc=self.key_desc, key_vals=key_vals, **other_kwargs)
 
     @staticmethod
-    def _schema_row_id():
+    def _schema_row_id() -> List[ColumnSpecType]:
         return [
             "key_desc",
             "key_vals",
