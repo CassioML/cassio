c341c9305350b03ace817b66dbceb09214289c53
Stefano Lottini <stefano.lottini@datastax.com>
2023-09-21 23:05:37 +0200

    bring (legacy) StoredBlobHistory to session-first init signature

--- src/cassio/history/stored_blob_history.py
+++ src/cassio/history/stored_blob_history.py
@@ -31,11 +31,7 @@ class StoredBlobHistory:
     )
 
     def __init__(
-        self,
-        table_name: str,
-        session: Optional[Session] = None,
-        keyspace: Optional[str] = None,
-        **kwargs: Dict[str, Any]
+        self, session: Session, keyspace: str, table_name: str, **kwargs: Dict[str, Any]
     ):
         #
         warn(self.DEPRECATION_MESSAGE, DeprecationWarning, stacklevel=2)
