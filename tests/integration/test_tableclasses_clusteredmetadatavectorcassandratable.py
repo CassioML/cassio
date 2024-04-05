441dde4dc7c64a09f5e9259377c80e30d672f2b5
Christophe Bornet <cbornet@hotmail.com>
2024-04-05 12:31:21 +0200

    Add cassandra tests with testcontainers (#144)
    * Add cassandra tests with testcontainers
    
    * Fine tune cassandra docker instance
    
    * failing test case on OSS C* more tightly circumscribed; better explanations around test modes
    
    * lint
    
    ---------
    
    Co-authored-by: Stefano Lottini <stefano.lottini@datastax.com>

--- tests/integration/test_tableclasses_clusteredmetadatavectorcassandratable.py
+++ tests/integration/test_tableclasses_clusteredmetadatavectorcassandratable.py
@@ -2,6 +2,8 @@
 Table classes integration test - ClusteredMetadataVectorCassandraTable
 """
 import math
+import os
+
 import pytest
 
 from cassandra import InvalidRequest
@@ -13,6 +15,9 @@ from cassio.table.tables import (
 
 
 N = 16
+
+
+TEST_DB_MODE = os.getenv("TEST_DB_MODE")
 
 
 @pytest.mark.usefixtures("db_session", "db_keyspace")
@@ -111,13 +116,90 @@ class TestClusteredMetadataVectorCassandraTable:
         ztheta_1n2 = t.get(row_id="theta_1", metadata={"odd": True}, partition_id=999)
         assert ztheta_1n2 is None
 
-        # ANN on 999
+        # cross-partition ANN search test
+        t_xpart = ClusteredMetadataVectorCassandraTable(
+            session=db_session,
+            keyspace=db_keyspace,
+            table=table_name,
+            vector_dimension=2,
+            primary_key_type=["INT", "TEXT"],
+            skip_provisioning=True,
+        )
+        # a vector at 1/4 step from the "_0" for both partitions
+        xp_query_theta = 1 * math.pi * 2 / (4 * N)
+        xp_vector = [math.cos(xp_query_theta), math.sin(xp_query_theta)]
+        xpart_results = list(
+            t_xpart.ann_search(
+                xp_vector,
+                n=2,
+            )
+        )
+        assert {r["row_id"] for r in xpart_results} == {"theta_0", "Z_theta_0"}
+        # "cross partition GET" (i.e. partition_id not specified).
+        # Outside of ANN this should throw an error
+        with pytest.raises(InvalidRequest):
+            _ = t.get(row_id="not_enough_info", partition_id=None)
+        with pytest.raises(InvalidRequest):
+            _ = t_xpart.get(row_id="not_enough_info")
+
+        t.clear()
+
+    @pytest.mark.skipif(
+        TEST_DB_MODE in {"LOCAL_CASSANDRA", "TESTCONTAINERS_CASSANDRA"},
+        reason="fails in Cassandra 5-beta1. To be reactivated once Cassandra is fixed.",
+    )
+    def test_crud_partitioned_ann(self, db_session: Session, db_keyspace: str) -> None:
+        table_name = "c_m_v_ct"
+        db_session.execute(f"DROP TABLE IF EXISTS {db_keyspace}.{table_name};")
+        #
+        # "INT" here means: partition_id is a number (for fun)
+        t = ClusteredMetadataVectorCassandraTable(
+            session=db_session,
+            keyspace=db_keyspace,
+            table=table_name,
+            vector_dimension=2,
+            primary_key_type=["INT", "TEXT"],
+            partition_id=0,
+        )
+
+        for n_theta in range(N):
+            theta = n_theta * math.pi * 2 / N
+            group = "odd" if n_theta % 2 == 1 else "even"
+            t.put(
+                row_id=f"theta_{n_theta}",
+                body_blob=f"theta = {theta:.4f}",
+                vector=[math.cos(theta), math.sin(theta)],
+                metadata={
+                    group: True,
+                    "n_theta_mod_2": n_theta % 2,
+                    "group": group,
+                },
+            )
+        # fill another partition (999 = "the other one")
+        for n_theta in range(N):
+            theta = n_theta * math.pi * 2 / N
+            group = "odd" if n_theta % 2 == 1 else "even"
+            t.put(
+                row_id=f"Z_theta_{n_theta}",
+                body_blob=f"Z_theta = {theta:.4f}",
+                vector=[math.cos(theta), math.sin(theta)],
+                partition_id=999,
+                metadata={
+                    group: True,
+                    "n_theta_mod_2": n_theta % 2,
+                    "group": group,
+                },
+            )
+
+        # ANN on partition 999
         # a vector halfway between 0 and 1 inserted above
         zquery_theta = 1 * math.pi * 2 / (2 * N)
         zref_vector = [math.cos(zquery_theta), math.sin(zquery_theta)]
+
         zann_results1 = list(t.ann_search(zref_vector, n=4, partition_id=999))
         assert {r["row_id"] for r in zann_results1[:2]} == {"Z_theta_1", "Z_theta_0"}
         assert {r["row_id"] for r in zann_results1[2:4]} == {"Z_theta_2", "Z_theta_15"}
+
         # ANN with metadata filtering
         zann_results_md1 = list(
             t.ann_search(zref_vector, n=4, metadata={"odd": True}, partition_id=999)
@@ -130,6 +212,7 @@ class TestClusteredMetadataVectorCassandraTable:
             "Z_theta_3",
             "Z_theta_13",
         }
+
         # and in another way...
         zann_results_md2 = list(
             t.ann_search(zref_vector, n=4, metadata={"group": "odd"}, partition_id=999)
@@ -159,31 +242,5 @@ class TestClusteredMetadataVectorCassandraTable:
             "Z_theta_3",
             "Z_theta_13",
         }
-
-        # cross-partition ANN search test
-        t_xpart = ClusteredMetadataVectorCassandraTable(
-            session=db_session,
-            keyspace=db_keyspace,
-            table=table_name,
-            vector_dimension=2,
-            primary_key_type=["INT", "TEXT"],
-            skip_provisioning=True,
-        )
-        # a vector at 1/4 step from the "_0" for both partitions
-        xp_query_theta = 1 * math.pi * 2 / (4 * N)
-        xp_vector = [math.cos(xp_query_theta), math.sin(xp_query_theta)]
-        xpart_results = list(
-            t_xpart.ann_search(
-                xp_vector,
-                n=2,
-            )
-        )
-        assert {r["row_id"] for r in xpart_results} == {"theta_0", "Z_theta_0"}
-        # "cross partition GET" (i.e. partition_id not specified).
-        # Outside of ANN this should throw an error
-        with pytest.raises(InvalidRequest):
-            _ = t.get(row_id="not_enough_info", partition_id=None)
-        with pytest.raises(InvalidRequest):
-            _ = t_xpart.get(row_id="not_enough_info")
 
         t.clear()
